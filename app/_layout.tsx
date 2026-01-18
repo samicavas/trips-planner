225c29f8617bd8e107be5c7e153e18d773e19771
Sami Çavaş <samicavas@192.168.1.15>
2026-01-18 18:29:18 +0300

    fixed layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,9 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import * as Linking from 'expo-linking';
 import { Stack } from "expo-router";
-import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  useEffect(() => {
-    const handleDeepLink = async (url: string) => {
-      try {
-        const urlObj = new URL(url);
-        const code = urlObj.searchParams.get('code');
-      } catch (error) {
-        console.error('Deep link error:', error);
-      }
-    };
-
-    const subscription = Linking.addEventListener('url', (event) => {
-      handleDeepLink(event.url);
-    });
-
-    Linking.getInitialURL().then((url) => {
-      if (url != null) {
-        handleDeepLink(url);
-      }
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
